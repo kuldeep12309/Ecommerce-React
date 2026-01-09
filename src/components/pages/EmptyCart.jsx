13ddb76523d70c738465283f7cd078d111d0d4e2
kuldeep gupta <kuldeepgupta8800@gmail.com>
2026-01-09 19:34:41 +0530

    minor change

--- src/components/pages/EmptyCart.jsx
+++ src/components/pages/EmptyCart.jsx
@@ -5,13 +5,13 @@ function EmptyCart() {
     const navigate = useNavigate();
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-50 text-gray-700">
+        <div className="flex flex-col items-center justify-center h-screen light:bg-gray-50 light:text-white">
             <img
                 src="https://cdn-icons-png.flaticon.com/512/2038/2038854.png"
                 alt="Empty Cart"
                 className="w-40 mb-6 opacity-70"
             />
-            <h2 className="text-2xl font-semibold mb-2">Oops! Your cart is empty.</h2>
+            <h2 className="text-2xl font-semibold mb-2 ">Oops! Your cart is empty.</h2>
             <p className="text-gray-500 mb-6">Looks like you haven’t added anything to your cart yet.</p>
             <button
                 onClick={() => navigate('/')}
