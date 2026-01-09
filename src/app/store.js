7e77a2c5bd2e2421d9082c0c4c587bdc811cd330
kuldeep gupta <kuldeepgupta8800@gmail.com>
2026-01-09 16:34:43 +0530

    minor change

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,46 @@
+
+
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cartSlice";
-import themeReducer from "./features/themeSlice";
+
+import themeReducer from "./features/themeSlice"
+
+
+const loadState = () => {
+    try {
+        const serializedState = localStorage.getItem("cart");
+        return serializedState ? JSON.parse(serializedState) : undefined;
+    } catch (err) {
+        return undefined;
+    }
+};
+
+
+const saveState = (state) => {
+    try {
+        const serializedState = JSON.stringify(state.cart);
+        localStorage.setItem("cart", serializedState);
+    } catch (err) {
+        console.error("Could not save state", err);
+    }
+};
+
+const preloadedState = {
+    cart: loadState() || { items: [] }
+};
 
 const store = configureStore({
     reducer: {
-    cart: cartReducer,
-    theme: themeReducer,
+        cart: cartReducer,
+        theme: themeReducer
+
+
     },
-    
+    preloadedState,
 });
 
+store.subscribe(() => {
+    saveState(store.getState());
+});
 
 export default store;
